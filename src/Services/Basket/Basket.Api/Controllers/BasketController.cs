bb937feb5e80b74df44d433243d74dc7004e44d5
Luis Ruiz <ruizluis41@hotmail.com>
2021-05-04 17:01:38 -0700

    Created a new Basket Api using Redis as the Data base, it will be a seperate micro controller to interact with rabbit MQ

---
+++ src/Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Basket.Api.Entities;
+using Basket.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        [HttpGet("{username}",Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket (string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            
+            //if basket is null create a new shopping cart 
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
