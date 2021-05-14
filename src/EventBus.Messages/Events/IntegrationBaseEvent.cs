39aee2724ead2815e099687144350915a47c6e63
Luis Ruiz <ruizluis41@hotmail.com>
2021-05-14 16:26:02 -0700

    Developing EventBus.Message

---
+++ src/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+
+        public Guid Id { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public IntegrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+        }
+
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+        }
+    }
+}
