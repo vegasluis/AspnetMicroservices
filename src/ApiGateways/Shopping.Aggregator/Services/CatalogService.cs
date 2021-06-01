203333722eb5e33c47cf734e56a98f52beedf63a
Luis Ruiz <ruizluis41@hotmail.com>
2021-06-01 10:45:20 -0700

    Developing Service Classes to Consume internal microservices in Shopping.Aggregrator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
@@ -0,0 +1,40 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _client;
+
+        public CatalogService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<CatalogModels> GetCatalog(string id)
+        {
+            var response = await _client.GetAsync($"/api/v1/Catalog/{id}");
+
+            return await response.ReadContentAs<CatalogModels>();
+        }
+
+        public async Task<IEnumerable<CatalogModels>> GetCatalogByCategory(string category)
+        {
+            var response = await _client.GetAsync($"/api/v1/Catalog/GetProductByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModels>>();
+        }
+
+        public async Task<IEnumerable<CatalogModels>> GetCatalog()
+        {
+            var response = await _client.GetAsync("/api/v1/Catalog");
+
+            return await response.ReadContentAs<List<CatalogModels>>();
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModels>> GetCatalog();
+        Task<IEnumerable<CatalogModels>> GetCatalogByCategory(string category);
+        Task<CatalogModels> GetCatalog(string id);
+    }
+}
